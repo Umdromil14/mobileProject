dcf24ff416a42c55b1d2b54c6b051aaf2bcf47b6
Cédric M <128589682+martcHenallux@users.noreply.github.com>
2023-11-23 12:37:33 +0100

    gamePreview et scrollView

---
+++ ProjectMobileV4.1/styles/globalStyles.jsx
@@ -0,0 +1,71 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+
+    container: {
+        padding: 20,
+    },
+    containerInsideView: {
+        width: Dimensions.get("window").width - 50,
+        marginTop: 20,
+        alignSelf: "center"
+    },
+    background : {
+        backgroundColor: '#463E3E',
+        flex: 1, 
+        alignItems: "center", 
+        justifyContent: "center"
+    },
+    button : {
+        backgroundColor : "#fff",
+        borderRadius: 10,
+        padding: 10,
+    },
+    inputContainer : {
+        paddingLeft: 16,
+        paddingRight: 16,
+        borderColor:'transparent',
+        backgroundColor:'#fff',
+        borderBottomLeftRadius: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    textForm : {
+        paddingTop: 12,
+        paddingLeft: 16,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+    button : {
+        backgroundColor: '#59A52C',
+        borderWidth: 0,
+        borderRadius: 20,
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+        alignSelf: "center",
+        marginTop: 25
+    },
+    error : {
+        color : '#cc0000',
+        fontSize : 16,
+        fontWeight : 'bold',
+
+    },
+    greenText: {
+        color : "#59A52C"
+    },
+    whiteText: {
+        color: "#fff"
+    },
+    modifyButtonContainer: {
+        width: 200,
+        alignSelf: "center",
+        marginBottom: 50,
+        marginTop: 15,
+        color: "#59A52C"
+    }
+});
